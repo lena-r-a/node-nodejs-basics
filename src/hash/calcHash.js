4d0d8f46efe1b9f3c520fd3868e7cf84d2ee246d
aryzhankova <rizhankova.elena@gmail.com>
2023-06-20 02:01:05 +0300

    add: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import { createHash } from "crypto"
+import fs from 'fs/promises'
+
 const calculateHash = async () => {
-    // Write your code here 
+   const read =  await fs.readFile('files/fileToCalculateHashFor.txt');
+   const hash = createHash("sha256").update(read).digest('hex');
+
+   console.log(hash);
 };
 
 await calculateHash();
