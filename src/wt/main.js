b039c00f498735e121e7fb00ee2619f05d535735
aryzhankova <rizhankova.elena@gmail.com>
2023-06-21 14:19:07 +0300

    add: implement threads tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from 'worker_threads'
+import os from 'os'
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPromises = [];
+    const numberOfsystemCpuCores = os.cpus().length;
+    const createWorker = (x) => {
+        return new Promise( (res,rej) => {
+            const worker = new Worker('./worker.js', {
+                workerData: 10 + x
+            });
+            worker.on('message', msg => {
+                res({'status': 'resolved', 'data' : msg})
+            })
+            worker.on('error', err => {
+                res({'status': 'error', 'data' : null})
+            })
+        }
+
+        )
+    }
+    for (let i=0; i<numberOfsystemCpuCores; i++) {
+        workerPromises.push(createWorker(i))
+    }
+    console.log(await Promise.all(workerPromises))
 };
 
 await performCalculations();
